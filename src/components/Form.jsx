9d3ca57a7203902b518a7551dfc4751d09f2380f
LOVKUSH <lovkushyadav9888@gmail.com>
2023-04-09 15:31:59 +0530

    realtime Db - showing in the firebase

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import { useState } from 'react'
+import {useAuthState} from 'react-firebase-hooks/auth'
+import {Auth, db} from './firebase-config'
+import {collection, addDoc} from 'firebase/firestore'
 
 const Form = () => {
-    const [message, setMessage] = useState("");
+    const [message, setMessage] = useState("")
+
+    const [user] = useAuthState(Auth)
+
+    const messageRef = collection (db, "messages")
+
     const handleSubmit = async (e) => {
         e.preventDefault()
         console.log(message);
+        if(user){
+          const res = await addDoc(messageRef, {
+            message: message,
+            user: user.displayName,
+            logo: user.photoURL
+          })
+          {/*res = response*/}
+          console.log(res);
+          alert ("send...")
+
+        }
     }
 
   return (
